8158ab331012bb52d1a1c08dac1d8e85292856a1
Anka <runner@iad20-fj924-e7440a4e-84cd-41b2-bcfe-4b621d5aaa77-7E8BE6936F23.local>
2025-10-28 09:49:33 +0000

    chore(release): update SPM definition

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-ae22aea1-a229-4198-838b-918327ab42f8.xcframework.zip"
+let remoteKotlinChecksum = "e1a5e6dcf5f4b62230090dccd52e6b6631eb73da844740932ee0660ec949b204"
+let packageName = "RealtimeKitFlutterCoreKMM"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: "RealtimeKitBridge",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: packageName, targets: [packageName, "RTKWebRTC"]),
+        .library(name: "RTKWebRTC", targets: ["RTKWebRTC"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RTKWebRTC",
+            url: "https://sdk-assets.realtime.cloudflare.com/RTKWebRTC-v125.6422.07.zip",
+            checksum: "114cb3ea15c5709f2c35d2b1c7a64e742a6902d375d54895984263bb79d75ce3"
+        ),
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        ),
+    ]
+)
