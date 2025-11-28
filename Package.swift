9ebef0197e8589dc3f8b0bd41c60ed43b461b3c9
Anka <runner@sjc20-bb711-f5159abc-7327-4a64-af21-83a827736253-96BFC28B5744.local>
2025-11-28 09:47:35 +0000

    chore(release): update SPM definition

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-395b25b6-f0f1-4f3a-bd1b-74b11776e2d8.xcframework.zip"
-let remoteKotlinChecksum = "9be57f842c7467542e332024aeafe39ce6b4bc4e28d8162bcdef54942f060e41"
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-046d8a18-a8e0-4c75-b752-86b49810cdb9.xcframework.zip"
+let remoteKotlinChecksum = "d17581c41947a74de4dbee8995e8fd026a96895b5b613c0a3fdd8b0ea5db7dbd"
 let packageName = "RealtimeKitFlutterCoreKMM"
 // END KMMBRIDGE BLOCK
 
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RTKWebRTC",
-            url: "https://sdk-assets.realtime.cloudflare.com/RTKWebRTC-v125.6422.07.zip",
-            checksum: "114cb3ea15c5709f2c35d2b1c7a64e742a6902d375d54895984263bb79d75ce3"
+            url: "https://sdk-assets.realtime.cloudflare.com/RTKWebRTC-137.7151.10.2.zip",
+            checksum: "2a56715d11dc3cbb722b43fc712fec68d9838b68b7297076e7ac3d0f8e9b6ccb"
         ),
         .binaryTarget(
             name: packageName,
