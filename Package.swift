39a8ce3312b8141494c573c049623868862eb64e
Anka <runner@iad20-fj920-bc17a07b-deab-40b0-8d65-e8473b36972a-06A8F10849C5.local>
2026-04-30 12:06:07 +0000

    chore(release): update SPM definition

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-95e8003f-2351-4cd5-8998-b9b88ff81289.xcframework.zip"
-let remoteKotlinChecksum = "2d65b48b1f13da49307093ce9297e1d45c23d06698f2675e2abe63133eeec206"
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-d4538202-2296-4176-be27-e4d204530d51.xcframework.zip"
+let remoteKotlinChecksum = "8924e522af1a65400de875b029e31e04d2079d17d87a99932c98df8c42f3f997"
 let packageName = "RealtimeKitFlutterCoreKMM"
 // END KMMBRIDGE BLOCK
 
